bbb5960d88e11f8b3492f1e30a380723fc78d473
William Porter <dawilster143@gmail.com>
2015-04-26 20:48:37 +1000

    Some events returned an array of params and some didn't. This accounts for both situations within the mapping

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,11 +4,16 @@ class Api::V1::EventsController < BaseApiController
   def mapped_params(params)
     mappings = {
       "name" => :sensor_type,
-      "linkText" => "display_name",
-      "value" => "value"
+      "linkText" => :display_name,
+      "value" => :value
     }
 
-    mapped = Hash[params[:event][0].map {|k, v| [mappings[k], v] }]
+    if params[:event].kind_of?(Array)
+      mapped = Hash[params[:event][0].map {|k, v| [mappings[k], v] }]
+    else
+      mapped = Hash[params[:event].map {|k, v| [mappings[k], v] }]
+    end
+
     return ActionController::Parameters.new(event: mapped)
   end
 
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,3 +1,4 @@
+
 require 'rails_helper'
 
 RSpec.describe Api::V1::EventsController, :type => :controller do
@@ -10,9 +11,20 @@ RSpec.describe Api::V1::EventsController, :type => :controller do
     }
   end
 
+  def event
+    {
+      "name"=>"power", "value"=>0.1, "isStateChange"=>false, "displayed"=>false, "linkText"=>"CentraLite Switch (Saeidur)", "descriptionText"=>{"bytes"=>[67, 101, 110, 116, 114, 97, 76, 105, 116, 101, 32, 83, 119, 105, 116, 99, 104, 32, 40, 83, 97, 101, 105, 100, 117, 114, 41, 32, 112, 111, 119, 101, 114, 32, 105, 115, 32, 48, 46, 49], "strings"=>["", " ", " is ", "", ""], "valueCount"=>4, "values"=>["CentraLite Switch (Saeidur)", "power", 0.1, ""]}
+    }
+  end
+
   describe "create" do
     it "returns 200" do
       post :create, format: :json, "event" => [temperature_event]
+      expect(response.response_code).to eq 201
+    end
+
+    it "returns 200" do
+      post :create, format: :json, event: event
       expect(response.response_code).to eq 201
     end
   end
