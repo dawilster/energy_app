fd658f074d1b594dc9b087d72ba182815662cae5
William Porter <dawilster143@gmail.com>
2015-05-20 14:13:39 +1000

    Add user_id and timestamp to admin columns

--- app/admin/survey.rb
+++ app/admin/survey.rb
@@ -1,5 +1,15 @@
 ActiveAdmin.register Survey do
 
+  index do
+    column :occupants
+    column :temperature_value
+    column :noise_level
+    column :comment
+    column :user_id
+    column :in_out
+    column(:timestamp) { |s| s.created_at }
+  end
+
   csv do
     column :occupants
     column :temperature_value
