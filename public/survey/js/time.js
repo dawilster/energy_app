4812c73b6c27e799966cdc20518f7885fe5e8b63
hengkeat <hengkeat11@hotmail.com>
2015-05-27 20:01:53 +1000

    Update time.js
    Add change date js

--- public/survey/js/time.js
+++ public/survey/js/time.js
@@ -28,3 +28,22 @@ function addtime(){
 	document.getElementById("time").innerHTML=time;
 	document.getElementById("timeinput").value = time;}
 }
+
+function inputDate(){
+	document.getElementById("date").style.display="none";
+	document.getElementById("changeDatebtn").style.display="none";
+	document.getElementById("usrdate").style.display="inline";
+	document.getElementById("showDatebtn").style.display="block";
+}
+
+function addDate(){
+	var date = document.getElementById("usrdate").value;
+	document.getElementById("date").style.display="inline";
+	document.getElementById("changeDatebtn").style.display="inline";
+	document.getElementById("usrdate").style.display="none";
+	document.getElementById("showDatebtn").style.display="none";
+	
+	if (date!= ""){
+	document.getElementById("date").innerHTML=date;
+	document.getElementById("dateinput").value = date;}
+}
